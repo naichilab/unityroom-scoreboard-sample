e227de3d901cab4da47715825947eab579015620
naichilab <naichilab@live.jp>
2023-04-28 12:52:29 +0900

    hmac-sha1

--- Assets/Scripts/SendScoreButton.cs
+++ Assets/Scripts/SendScoreButton.cs
@@ -27,16 +27,38 @@ public class SendScoreButton : MonoBehaviour
 
     async UniTask SendScore(float score)
     {
-        WWWForm form = new WWWForm();
-        form.AddField("score", score.ToString());
+        var scoreboardId = 1;
+        var url = $"http://219.play.lvh.me:3000/api/v1/scoreboards/{scoreboardId}/scores";
 
-        using var request = UnityWebRequest.Post("http://219.play.lvh.me:3000/api/v1/scoreboards/1/scores",form);
-        request.SetRequestHeader("X-Unityroom-Timestamp", GetUnixTime().ToString());
-        request.SetRequestHeader("X-Unityroom-Signature", "1udfwegfuhqwdfqe");
-        request.SetRequestHeader("X-Unityroom-Id", SystemInfo.deviceUniqueIdentifier);
+        var timestampText = GetUnixTime()
+            .ToString();
+        var scoreText = score.ToString();
+
+        var sha1Text = sha1($"{timestampText}:{scoreText}", "12345");
+        
+        WWWForm form = new WWWForm();
+        form.AddField("score", scoreText );
+
+        using var request = UnityWebRequest.Post(url,form);
+        request.SetRequestHeader("X-Unityroom-Timestamp", timestampText);
+        request.SetRequestHeader("X-UNITYROOM-HMAC-SHA1", sha1Text);
 
         await request.SendWebRequest();
         Debug.Log(request.downloadHandler.text);
+    }
+    
+    string sha1(string planeStr, string key) {
+        UTF8Encoding ue = new UTF8Encoding();
+        byte[] planeBytes = ue.GetBytes(planeStr);
+        byte[] keyBytes = ue.GetBytes(key);
+
+        System.Security.Cryptography.HMACSHA1 sha1 = new System.Security.Cryptography.HMACSHA1(keyBytes);
+        byte[] hashBytes = sha1.ComputeHash(planeBytes);
+        string hashStr = "";
+        foreach(byte b in hashBytes) {
+            hashStr += $"{b,0:x2}";
+        }
+        return hashStr;
     }
 
     private static DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0);
