bd81eeaa11c25ff8e6319708d23baebafb830be5
naichilab <naichilab@live.jp>
2023-04-29 01:03:46 +0900

    スコア送信

--- Assets/Scripts/SendScoreButton.cs
+++ Assets/Scripts/SendScoreButton.cs
@@ -1,6 +1,6 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -9,72 +9,63 @@ using Random = UnityEngine.Random;
 
 public class SendScoreButton : MonoBehaviour
 {
-    
+    [SerializeField]
+    private Button Button;
+
     void Start()
     {
-     
-
-        var button = GetComponent<Button>();
-        
-        button.onClick.AddListener(
-            () =>
-            {
-                StartCoroutine(SendScore2(Random.Range(0, 100)));
-            });
+        Button.onClick.AddListener(OnClick);
     }
 
- 
-    IEnumerator SendScore2(float score) {
-        var scoreboardId = 1;
-        var url = $"http://219.play.lvh.me:3000/api/v1/scoreboards/{scoreboardId}/scores";
+    void OnClick()
+    {
+        var scoreBoardId = 1;
+        var authenticationKey = "kr/2mTwV6XQsOPCFqpN3lw==";
+        var score = Random.Range(0, 100);
 
-        var timestampText = GetUnixTime()
-            .ToString();
-        var scoreText = score.ToString();
+        //スコア送信
+        StartCoroutine(SendScore(scoreBoardId, score, authenticationKey));
+    }
 
-        var sha1Text = sha1($"{timestampText}:{scoreText}", "kr/2mTwV6XQsOPCFqpN3lw==");
-        
-        WWWForm form = new WWWForm();
-        form.AddField("score", scoreText );
+    IEnumerator SendScore(
+        int scoreboardId
+        , float score
+        , string authenticationKey
+    )
+    {
+        if (IsEditor())
+        {
+            Debug.Log($"スコア送信 scoreboardId={scoreboardId} score={score} (unityroomにゲームをアップロードすると実際に送信されます)");
+            yield break;
+        }
 
-        using var request = UnityWebRequest.Post(url,form);
-        request.SetRequestHeader("X-Unityroom-Timestamp", timestampText);
-        request.SetRequestHeader("X-UNITYROOM-HMAC-SHA1", sha1Text);
-
+        var url = $"/api/v1/scoreboards/{scoreboardId}/scores";
+        var scoreText = score.ToString(CultureInfo.InvariantCulture);
+        var hmac = SHA1(scoreText, authenticationKey);
+        var form = new WWWForm();
+        form.AddField("score", scoreText);
+        using var request = UnityWebRequest.Post(url, form);
+        request.SetRequestHeader("X-Unityroom-Signature", hmac);
         yield return request.SendWebRequest();
- 
-        if (request.result != UnityWebRequest.Result.Success) {
-            Debug.Log(request.error);
-        }
-        else {
-            Debug.Log(request.downloadHandler.text);
-        }
-    }
-    
-    string sha1(string planeStr, string key) {
-        UTF8Encoding ue = new UTF8Encoding();
-        byte[] planeBytes = ue.GetBytes(planeStr);
-        byte[] keyBytes = ue.GetBytes(key);
-
-        System.Security.Cryptography.HMACSHA1 sha1 = new System.Security.Cryptography.HMACSHA1(keyBytes);
-        byte[] hashBytes = sha1.ComputeHash(planeBytes);
-        string hashStr = "";
-        foreach(byte b in hashBytes) {
-            hashStr += $"{b,0:x2}";
-        }
-        return hashStr;
+        Debug.Log(request.result == UnityWebRequest.Result.Success ? request.downloadHandler.text : request.error);
     }
 
-    private static DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0);
-
-    public static long GetUnixTime()
+    private static string SHA1(string text, string key)
     {
-        return (long)(DateTime.Now - UnixEpoch).TotalSeconds;
+        var utf8 = new UTF8Encoding();
+        var planeBytes = utf8.GetBytes(text);
+        var keyBytes = utf8.GetBytes(key);
+        var sha1 = new System.Security.Cryptography.HMACSHA1(keyBytes);
+        var hashBytes = sha1.ComputeHash(planeBytes);
+        return hashBytes.Aggregate("", (current, b) => current + $"{b,0:x2}");
     }
 
-    // UnixTimeからDateTimeへ変換
-    public static DateTime GetDateTime(long unixTime)
+    private bool IsEditor()
     {
-        return UnixEpoch.AddSeconds(unixTime);
+#if UNITY_EDITOR
+        return true;
+#else
+        return false;
+#endif
     }
 }
