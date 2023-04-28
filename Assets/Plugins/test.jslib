383d60bcf71da1b6b7159a145dd9c77b2333a2d9
naichilab <naichilab@live.jp>
2023-04-28 11:48:54 +0900

    送信

--- Assets/Plugins/test.jslib
+++
@@ -1,9 +0,0 @@
-mergeInto(LibraryManager.library, {
-  GetCsrfTokenInternal: function () {
-    var csrfToken = GetCsrfToken();
-    var bufferSize = lengthBytesUTF8(csrfToken) + 1;
-    var buffer = _malloc(bufferSize);
-    stringToUTF8(csrfToken, buffer, bufferSize);
-    return buffer;
-  },
-});
--- Assets/Scripts/SendScoreButton.cs
+++ Assets/Scripts/SendScoreButton.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Runtime.InteropServices;
+using System.Text;
 using Cysharp.Threading.Tasks;
 using UniRx;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class SendScoreButton : MonoBehaviour
 {
@@ -11,26 +14,41 @@ public class SendScoreButton : MonoBehaviour
     void Start()
     {
         var button = GetComponent<Button>();
-
+        
         button.OnClickAsObservable()
             .Subscribe(
                 async _ =>
                 {
-                    var csrfToken = GetCsrfTokenInternal();
-                    
-                    WWWForm form = new WWWForm();
-                    form.AddField("score", Random.Range(0,100));
-
-                    using var request = UnityWebRequest.Post("/api/v1/scoreboards/5/scores",form);
-                    request.SetRequestHeader("X-CSRF-TOKEN", csrfToken);
-
-                    await request.SendWebRequest();
-                    Debug.Log(request.responseCode);
+                    await SendScore(Random.Range(0, 100));
                 }
             )
             .AddTo(this);
     }
 
-    [DllImport("__Internal")]
-    private static extern string GetCsrfTokenInternal();
+    async UniTask SendScore(float score)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("score", score.ToString());
+
+        using var request = UnityWebRequest.Post("http://219.play.lvh.me:3000/api/v1/scoreboards/1/scores",form);
+        request.SetRequestHeader("X-Unityroom-Timestamp", GetUnixTime().ToString());
+        request.SetRequestHeader("X-Unityroom-Signature", "1udfwegfuhqwdfqe");
+        request.SetRequestHeader("X-Unityroom-Id", SystemInfo.deviceUniqueIdentifier);
+
+        await request.SendWebRequest();
+        Debug.Log(request.downloadHandler.text);
+    }
+
+    private static DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+
+    public static long GetUnixTime()
+    {
+        return (long)(DateTime.Now - UnixEpoch).TotalSeconds;
+    }
+
+    // UnixTimeからDateTimeへ変換
+    public static DateTime GetDateTime(long unixTime)
+    {
+        return UnixEpoch.AddSeconds(unixTime);
+    }
 }
