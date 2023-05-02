7a9189395031a10854b4dd4f64ce9d9822a93712
naichilab <naichilab@live.jp>
2023-05-02 23:17:17 +0900

    リクエストにTimeStampを付与

--- Assets/Scripts/SendScoreButton.cs
+++ Assets/Scripts/SendScoreButton.cs
@@ -21,9 +21,11 @@ public class SendScoreButton : MonoBehaviour
     void OnClick()
     {
         //unityroomのスコアボード設定ページにある"ボードNo"を使用してください。
-        var boardNo = 1; 
+        var boardNo = 1;
+
         //unityroomのスコアボード設定ページにある"認証用キー"を使用してください。
-        var authenticationKey = "5WCfLECeGOOUwSiThshDGWznn+eM9mVlRSX4BbDmIzhbhi8JhH7jHcFERUCqV++IkxpygP+8n+qDGlVDGyH7OQ=="; 
+        var authenticationKey
+            = "4WXyUpqaDws8VZL1LsixwooVP+9kDgAmXQMB63wWOl5s271rhZcBa0irVeTlda/zAtuvcgBfEWj3oZ4FptQmjg==";
 
         //獲得スコア
         var score = Random.Range(0, 100);
@@ -44,31 +46,63 @@ public class SendScoreButton : MonoBehaviour
             yield break;
         }
 
-        var url = $"/gameplay_api/v1/scoreboards/{boardNo}/scores";
+        // 送信するスコアを文字列に変換しておく
         var scoreText = score.ToString(CultureInfo.InvariantCulture);
-        var hmac = SHA256(scoreText, authenticationKey);
+
+        // 現在のUNIXTIMEを取得
+        var unixtime = GetCurrentUnixTime()
+            .ToString();
+
+        // 認証用のHMAC(Hash-based Message Authentication Code)を計算する
+        var hmacDataText = $"{unixtime}:{scoreText}";
+        var hmac = GetHmacSha256(hmacDataText, authenticationKey);
+
+        // APIリクエストを送信する
+        // スコアはFormDataとして付与する
+        // 認証用の情報はリクエストヘッダーに付与する
+        var uri = $"/gameplay_api/v1/scoreboards/{boardNo}/scores";
         var form = new WWWForm();
         form.AddField("score", scoreText);
-        using var request = UnityWebRequest.Post(url, form);
+        using var request = UnityWebRequest.Post(uri, form);
         request.SetRequestHeader("X-Unityroom-Signature", hmac);
+        request.SetRequestHeader("X-Unityroom-Timestamp", unixtime);
         yield return request.SendWebRequest();
+
+        //結果をログに表示
         Debug.Log($"{request.responseCode}|{request.downloadHandler.text}");
     }
 
-    private static string SHA256(string text, string base64Key)
+    /// <summary>
+    /// SHA256を用いてHMACを計算する
+    /// </summary>
+    /// <param name="dataText">計算対象文字列</param>
+    /// <param name="base64AuthenticationKey">HMACSHA256に用いるキー（base64）</param>
+    /// <returns>HMAC SHA-256</returns>
+    private static string GetHmacSha256(string dataText, string base64AuthenticationKey)
     {
-       
-        var utf8 = new UTF8Encoding(false); //BOM無しUTF-8
-        var textBytes = utf8.GetBytes(text);
-        var keyBytes = Convert.FromBase64String(base64Key);
-        var sha256 = new System.Security.Cryptography.HMACSHA256(keyBytes);
-        var hashBytes = sha256.ComputeHash(textBytes);
-
-        //byte型配列を16進数に変換
-        var hash = BitConverter.ToString(hashBytes).ToLower().Replace("-", "");
-        return hash;
+        var dataBytes = new UTF8Encoding(false).GetBytes(dataText); // BOM無しUTF-8のbyte配列に変換
+        var keyBytes = Convert.FromBase64String(base64AuthenticationKey); // base64の認証用キーをデコードしてbyte配列に変換
+        var sha256 = new System.Security.Cryptography.HMACSHA256(keyBytes); // ハッシュ関数はSHA256を用いる
+        var hmacBytes = sha256.ComputeHash(dataBytes); // ハッシュ値を計算
+        var hmacText = BitConverter.ToString(hmacBytes)
+            .ToLower()
+            .Replace("-", ""); // byte型配列を16進数の文字列に変換
+        return hmacText;
     }
 
+    /// <summary>
+    /// 現在時刻のUnixTimeを返す
+    /// </summary>
+    /// <returns></returns>
+    public static int GetCurrentUnixTime()
+    {
+        return (int)(DateTime.UtcNow.Subtract(DateTime.UnixEpoch)).TotalSeconds;
+    }
+
+    /// <summary>
+    /// Unityエディタで実行中かどうか
+    /// </summary>
+    /// <returns></returns>
     private bool IsEditor()
     {
 #if UNITY_EDITOR
